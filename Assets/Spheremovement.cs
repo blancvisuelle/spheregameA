6b6a456916ae40f32a7e3332ed581a50ccb0791a
blancvisuelle <dimaswidyabakti@gmail.com>
2020-10-03 13:12:48 +0800

    Initial Commit

---
+++ Assets/Spheremovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spheremovement : MonoBehaviour
+{
+    public GameObject sphere1;
+    public int loopCount = 1;
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key is pressed.");
+            if (loopCount % 2 != 0)
+            {
+                sphere1.transform.eulerAngles = new Vector3(0, 45, 0);
+                loopCount++;
+            }
+            
+            else
+            {
+                sphere1.transform.eulerAngles = new Vector3(0, -45, 0);
+                loopCount++;
+            }
+        }
+    }
+}
